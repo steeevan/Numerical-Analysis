6006bbc6429f2d0210a3f053fd7efae6329556aa
steeevan <estevananguiano99@yahoo.com>
2024-09-13 13:22:51 -0700

    Week 2 Programming Done

--- MAT4010 Analysis/Week2/H02_Q02.m
+++ MAT4010 Analysis/Week2/H02_Q02.m
@@ -1,57 +1,58 @@
 function [] = H02_Q02()
-    
+    % H02_Q02 - A script that applies the Bisection Method to find a root
+    % of a given function in a user-specified interval [a, b] with a desired
+    % tolerance.
+    %
+    % This function prompts the user to input values for the interval endpoints
+    % 'a' and 'b', and the tolerance 'epsilon', then uses the bisection method
+    % to find the root of the function f(x) = x^2 - 5.
+    %
+    % Usage:
+    %   Call the function without any arguments:
+    %   >> H02_Q02
+    %
+    % User Inputs:
+    %   a - Left endpoint of the interval [a, b]
+    %   b - Right endpoint of the interval [a, b]
+    %   epsilon - Desired accuracy for the root
+
+    % Define the function handle for f(x) = x^2 - 5
     f = @(x) x^2 - 5;
     
+    % Prompt the user for input values
     a = input('Enter a value for a: ');
     b = input('Enter a value for b: ');
-    ep= input('Enter a value for epsilon: ');
-
-    bisection(a,b,f,ep);
+    ep = input('Enter a value for epsilon:');
     
+    % Call the bisection function to find the root
+    bisection(a, b, f, ep);
 end
 
-
- %{
-function [] = bisection(a,b,f,ep)
-   
-    an(1) = a;
-    bn(1) = b;
-    nMax = ceil(log2((b-a)/ep));
-    fprintf('This is nMax-> %0.2f\n',nMax);
-    
-    %p = zeros(1,nMax);
-
-    for n=1:nMax
-
-        pn(n) = (an(n) + bn(n)) / 2;
-
-        if sign(f(an(n))) ~= sign(f(bn(n)))
-            an(n+1) = an(n);
-            bn(n+1) = pn(n);
-        else
-            an(n+1) = pn(n);
-            bn(n+1) = bn(n);
-        end
-
-        fprintf('n = %d: an = %f, pn = %f, bn = %f, sign(f(an)) = %d, sign(f(pn)) = %d, sign(f(bn)) = %d\n',n,an(n),pn(n),bn(n),sign(f(an(n))),sign(f(pn(n))),sign(f(bn(n))));
-    end
-   %}
 function [] = bisection(a, b, f, ep)
-    % Bisection Method to find root of function f in the interval [a, b]
-    % a - starting point of the interval
-    % b - end point of the interval
-    % f - function handle
-    % ep - desired tolerance (epsilon)
-
-    nMax = ceil(log2((b - a) / ep));  % Maximum iterations needed
-    fprintf('This is nMax -> %d\n', nMax);
+    % Bisection Method to find the root of the function f in the interval [a, b]
+    %
+    % This function applies the Bisection Method iteratively to find a root
+    % of the given function f with an accuracy specified by epsilon.
+    %
+    % Inputs:
+    %   a  - Left endpoint of the interval [a, b]
+    %   b  - Right endpoint of the interval [a, b]
+    %   f  - Function handle for the equation to be solved
+    %   ep - Desired tolerance (epsilon) for the root accuracy
+    %
+    % Output:
+    %   Prints each iteration showing the interval and midpoint with the
+    %   sign of f(a), f(midpoint), and f(b).
+    
+    % Calculate maximum number of iterations needed for the specified tolerance
+    nMax = ceil(log2((b - a) / ep));
     
     % Pre-allocate space for variables
-    p = zeros(1, nMax);   % Midpoints
-    a_values = zeros(1, nMax);  % a(n) values
-    b_values = zeros(1, nMax);  % b(n) values
+    p = zeros(1, nMax);       % Midpoints
+    a_values = zeros(1, nMax); % Left endpoints (a_n values)
+    b_values = zeros(1, nMax); % Right endpoints (b_n values)
     
-    % Store initial values
+    % Store initial values of a and b
     a_values(1) = a;
     b_values(1) = b;
     
@@ -64,10 +65,8 @@ function [] = bisection(a, b, f, ep)
         b_values(n) = b;
 
         % Print the iteration details
-        fprintf('n = %d: a = %f, p = %f, b = %f, f(a) = %f, f(p) = %f, f(b) = %f\n', ...
-            n, a, p(n), b, f(a), f(p(n)), f(b));
-
-       
+        fprintf('n = %d: an = %0.8f, pn = %0.8f, bn = %0.8f, sign(f(an)) = %d, sign(f(pn)) = %d, sign(f(bn)) = %d\n', ...
+            n, a, p(n), b, sign(f(a)), sign(f(p(n))), sign(f(b)));
 
         % Determine which subinterval to select
         if sign(f(a)) ~= sign(f(p(n)))
@@ -80,9 +79,9 @@ function [] = bisection(a, b, f, ep)
         a_values(n+1) = a;
         b_values(n+1) = b;
     end
-    
+
     % Final result if the loop completes
-    fprintf('Approximate root after %d iterations: %f\n', nMax, p(n));
+    %fprintf('Approximate root after %d iterations: %f\n', nMax, p(n));
 
     %% Plot 1: a(n), p(n), and b(n) versus n
     figure;
@@ -132,11 +131,20 @@ function [] = bisection(a, b, f, ep)
 
         figure;
         plot(3:nMax, alpha, 'm*-', 'LineWidth', 2);
-        xlabel('Iteration (n)');
-        ylabel('$\bar{\alpha}_n$', 'interpreter', 'latex');
-        title('Convergence rate approximation \bar{\alpha}_n vs n');
+         % Set axis limits
+        xlim([0 20]);
+        ylim([0 1.01]);
+        
+        % Set tick steps
+        xticks(0:5:20);  % 
+        yticks(0: 1.01);  % Ticks
+        ylabel('Iteration (n)');
+        xlabel( '$\bar{e} {n-1}$', 'interpreter', 'latex' );
+        title('Bisection Method for f(x) = x^2 - 5');
         grid on;
     else
         fprintf('Not enough error bounds to compute \alpha values.\n');
     end
+    
+
 end
